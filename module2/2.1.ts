f3698bef523ee8e82fcd0f0a0c513a1895445325
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-07 14:16:51 +0600

    Type assertion

---
+++ module2/2.1.ts
@@ -0,0 +1,38 @@
+{
+//
+//Type assertion
+let anything : any;
+anything = "Next Level Web Dev";
+
+anything = 222;
+
+//(anything as number).
+
+const KgToGm = (value:string | number):string | number | undefined => {
+    if(typeof value ==='string'){
+        const convertedValue = parseFloat(value)*1000
+        return `The converted value is: ${convertedValue}`
+    }
+    if(typeof value ==='number'){
+        
+        return value*1000
+    }
+
+}
+const result1 = KgToGm(1000) as number
+const result2 = KgToGm("1000") as string
+
+type CustomError = {
+    message: string;
+}
+
+try{
+
+}
+catch(error){
+console.log((error as CustomError) )
+}
+
+
+//
+}
