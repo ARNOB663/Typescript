2584355a3a871b76a160b57d39e14917b63011f6
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-07 17:59:10 +0600

    Create 2.2_Interface.ts

---
+++ module2/2.2_Interface.ts
@@ -0,0 +1,24 @@
+{
+//
+//interface
+type User1 =  {
+    name: string;
+    age: number;   
+}
+
+interface User2 {
+    name: string,
+    age: number
+}
+
+const user1:  User1 ={
+    name:'Arnob',
+    age:100,
+}
+
+
+
+
+
+//
+}
