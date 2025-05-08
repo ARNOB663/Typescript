27c78faf0a1b200e6ae7a38df79ea87a971ce490
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-08 19:50:58 +0600

    Funtion with generics

---
+++ module2/2.5_Funtion_with_generics.ts
@@ -0,0 +1,47 @@
+{
+//Funtion with generics
+const createArray = (param:string): string[] =>{
+    return [param]
+}
+const createArrayWithGeneric = <T>(param: T ) : T[] =>{
+    return [param]
+}
+
+type User1 = {
+    id:number;name:string
+}
+
+const res1 = createArray('Bangladesh');
+const  resGeneric = createArrayWithGeneric<string>('UK');
+const resGenericObj =  createArrayWithGeneric<User>({id:222,name:"arnob"})
+//
+
+const createArrayWithGenericTuple = <T,Q>(param1: T,param2:Q ) : [T,Q] =>{
+    return [param1,param2]
+}
+
+type User = {
+    id:number;name:string
+}
+
+const res2 = createArrayWithGenericTuple<string, number>('Bangladesh',12);
+const  resGeneric1 = createArrayWithGenericTuple<string,{name:string}>('UK',{name:"asia"});
+//const resGenericObj1 =  createArrayWithGenericTuple<User>({id:222,name:"arnob"},)
+
+
+const  addcourseToStudent = <T>( student : T ) =>{
+const course  = "Next Level Web Development"
+ 
+return {
+  ...student,
+  course
+}
+}
+
+const student1 = addcourseToStudent({name:'Mr X',email:"x@gmail.com",devType:"NLW"})
+
+const student2 = addcourseToStudent({name:'Mr Y',email:"x@gmail.com",devType:"NLW",hasWacth:"Apple Watch"})
+
+//
+
+}
