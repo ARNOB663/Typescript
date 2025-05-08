e84cb35fdf84d5e0a9f6a0c55df75b556d5d3ac7
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-05-08 17:07:22 +0600

    Generic type

--- module2/2.2_Interface.ts
+++ module2/2.2_Interface.ts
@@ -4,21 +4,50 @@
 type User1 =  {
     name: string;
     age: number;   
-}
+};
 
 interface User2 {
     name: string,
     age: number
-}
+};
 
-const user1:  User1 ={
+ interface UserWithRole2 extends  User2 {
+    role: string
+ }
+
+ type UserWithRole1 = User1 & {role : string}
+
+ 
+ const user1:  UserWithRole1 ={
     name:'Arnob',
     age:100,
+    role: "",
+};
+
+const user2:  UserWithRole2={
+    name:'Arnob',
+    age:100,
+    role:"Tester"
+};
+// type rollNumber = number {
+
+// }
+
+// js--> Object ,array-> object funtion -> object
+type Roll = number[];
+
+interface Roll2 {
+   [index: number] : number //arry decalre using interface
+
+}
+const rollNumbers1: Roll = [1, 2, 3, 4]; 
+
+interface Add {
+    (num1:number,num2:number) : number // retun type
 }
 
-
-
-
+//type Add =  (num1:number,num2:number) =>number
+const add:Add = (num1,num2) => num1+num2; 
 
 //
 }
---
+++ module2/2.3_Generics.ts
@@ -0,0 +1,54 @@
+{
+// generic type
+
+type GenericArray = Array<number>
+type GenericArrayBool = Array<boolean>
+
+type GenericArrayParams<T> = Array<T>
+
+//const rollNumbers : number[] = [3,6,8]
+//const rollNumbers : Array<number> = [3,6,8]
+//const rollNumbers : GenericArray = [3,6,8]
+const rollNumbers : GenericArrayParams<number> = [3,6,8]
+
+//const mentors: string[] = ["Mr. X","Mr.Y","Mr. Z"]
+
+//const mentors: Array<string> = ["Mr. X","Mr.Y","Mr. Z"]
+
+
+const mentors: GenericArrayParams<string> = ["Mr. X","Mr.Y","Mr. Z"]
+
+//const boolArray: boolean[] = [true,false,true];
+//const boolArray: Array<boolean> = [true,false,true];
+
+
+const user: GenericArrayParams<{name:string,age:number}> = [{
+  name: "Arnob",
+  age: 50,
+},{
+   name:"Daddy",
+   age:60
+}]
+
+
+const boolArray: GenericArrayBool = [true,false,true];
+
+const add = (x:number,y:number)=> x + y;
+
+add(30,20)
+type GenericTuple<X,Y> = [X,Y]
+
+//generic tuple 
+const manush:GenericTuple<string,string> = ['Mr. X','Mr. Y']
+
+const UserWithID: GenericTuple<number,{name:string,email:string}> = [1234,{name:'Arnob',email:"a@gmail.com"}]
+
+
+
+
+
+
+
+
+//
+}
